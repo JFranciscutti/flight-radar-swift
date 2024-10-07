6d37ebd622efc825c52ce7951af25748dfd08af8
Joaquin Franciscutti <joaco.1999.13@gmail.com>
2024-10-07 17:21:07 -0300

    Initial Commit

---
+++ Flight Radar V1/Flight_Radar_V1App.swift
@@ -0,0 +1,17 @@
+//
+//  Flight_Radar_V1App.swift
+//  Flight Radar V1
+//
+//  Created by Joaquin Franciscutti on 07/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Flight_Radar_V1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Flight Radar V1Tests/Flight_Radar_V1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Flight_Radar_V1Tests.swift
+//  Flight Radar V1Tests
+//
+//  Created by Joaquin Franciscutti on 07/10/2024.
+//
+
+import Testing
+@testable import Flight_Radar_V1
+
+struct Flight_Radar_V1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
